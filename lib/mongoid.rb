7e67839e2553c1bd2abdd765444b647f46ca9e59
Durran Jordan <durran@gmail.com>
2012-03-08 16:15:39 +0100

    Integrate Origin into Mongoid.

--- lib/mongoid.rb
+++ lib/mongoid.rb
@@ -20,15 +20,13 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-require "delegate"
-require "time"
 require "active_support/core_ext"
 require 'active_support/json'
 require "active_support/inflector"
-require "active_support/lazy_load_hooks"
 require "active_support/time_with_zone"
 require "active_model"
 require "mongo"
+require "origin"
 require "mongoid/extensions"
 require "mongoid/errors"
 require "mongoid/safety"
